3b4431ea5c543a77b483ddfe6094a37bcaef7a65
akenoq <akenoq@yandex.ru>
2017-11-17 00:03:57 +0300

    deploed 3d sw server & client + basic example

---
+++ 3d-world-basic/static/scripts/myScripts/MainClass.js
@@ -0,0 +1,35 @@
+"use strict";
+
+import SceneWorker from "./SceneWorker.js";
+import ElementGetter from "./ElementGetter.js";
+import Logger from "./Logger.js";
+
+class MainClass {
+    constructor(){
+        Logger.write("Version: 1.2");
+        this.createControlingObjects();
+        this.addEventsToPanelButtons();
+    }
+
+    createControlingObjects(){
+        this.sceneWorker = new SceneWorker("threeJSGraphicsBox", 1000, 800);
+    }
+
+    addEventsToPanelButtons(){
+        const t = this;
+
+        ElementGetter.get("btnTop").onclick = function () {
+            Logger.write("Proection TOP");
+            t.sceneWorker.setCameraType("TOP");
+        };
+
+        ElementGetter.get("btnFace").onclick = function () {
+            Logger.write("Proection FACE");
+            t.sceneWorker.setCameraType("FACE");
+        }
+    }
+}
+
+window.onload = function(){
+    const mainObj = new MainClass();
+};
