9c40bfe708d85873ed282f17677d4b691ede721e
akenoq <akenoq@yandex.ru>
2017-11-25 01:29:07 +0300

    график: автопрокрутка + масштаб

--- neuro-console-ws-client/static/code.js
+++ neuro-console-ws-client/static/code.js
@@ -1,9 +1,19 @@
 "use strict";
 
+//////////////////////////////
+
+let xxx_1 = 0;
+let xxx_2 = 0;
+let xxx_3 = 0;
+
+//////////////////////////////
+
 let can = null;
 let holst = null;
 
-let mass = [];
+let mass_1 = [];
+let mass_2 = [];
+let mass_3 = [];
 
 function drawFon() {
     holst.fillRect(-20, -20, 1050, 450);
@@ -22,11 +32,27 @@ function drawLine(x1, y1, x2, y2) {
     holst.stroke();
 }
 
-function addPoint(xx, yy) {
-    mass.push({
-        xx: xx,
-        yy: yy
-    });
+function addPoint(xx, yy, number) {
+    if(number === 1) {
+        mass_1.push({
+            xx: xx,
+            yy: yy
+        });
+    }
+
+    if(number === 2) {
+        mass_2.push({
+            xx: xx,
+            yy: yy
+        });
+    }
+
+    if(number === 3) {
+        mass_3.push({
+            xx: xx,
+            yy: yy
+        });
+    }
 }
 
 
@@ -47,9 +73,7 @@ window.onload = function() {
     holst = can.getContext('2d');
 
     holst.lineWidth = 1;
-    holst.fillStyle = '#33dfba';
-    holst.strokeStyle = '#0f4fff';
-
+    holst.fillStyle = '#512142';
 
     let myInter = setInterval(() =>{
         t_x = parseFloat(elem("t_x").value);
@@ -79,17 +103,44 @@ window.onload = function() {
         } catch (err) { }
 
 
+        if(type === "STOP") {
+            dx = -xxx_1 * t_x + 800;
+        }
+
         drawFon();
         drawGarphics();
     }, 100);
 };
 
-function f(xx) {
-    return Math.tan(xx);
-}
-
 function drawGarphics() {
+    let mass = null;
 
+    mass = mass_1;
+    holst.strokeStyle = '#FF0000';
+    for(let i = 0; i < mass.length; i++) {
+        if(i !== 0) {
+            const x1 = mass[i - 1].xx;
+            const y1 = mass[i - 1].yy;
+            const x2 = mass[i].xx;
+            const y2 = mass[i].yy;
+            drawLine(x1, y1, x2, y2);
+        }
+    }
+
+    mass = mass_2;
+    holst.strokeStyle = '#ffee00';
+    for(let i = 0; i < mass.length; i++) {
+        if(i !== 0) {
+            const x1 = mass[i - 1].xx;
+            const y1 = mass[i - 1].yy;
+            const x2 = mass[i].xx;
+            const y2 = mass[i].yy;
+            drawLine(x1, y1, x2, y2);
+        }
+    }
+
+    mass = mass_3;
+    holst.strokeStyle = '#0000FF';
     for(let i = 0; i < mass.length; i++) {
         if(i !== 0) {
             const x1 = mass[i - 1].xx;
@@ -104,8 +155,6 @@ function drawGarphics() {
 
 ////////////////////////////////////
 
-let xxx = 0;
-
 socket.onopen = function() {
     console.log("Соединение установлено");
 };
@@ -116,7 +165,6 @@ socket.onclose = function(event) {
 
 socket.onmessage = function(event) {
     console.log("Получено сообщение: " + event.data);
-
     let message = event.data.toString();
 
     if(message === "FIRST" || message === "SECOND") {
@@ -129,19 +177,52 @@ socket.onmessage = function(event) {
     }
 
     if(message.indexOf("@@@") === -1) {
-        let arr = message.split(" ");
+        let mmm = message.split("XXX");
+        let m_1_str = mmm[0].toString();
+        let ost = mmm[1];
+        let kkk = ost.split("YYY");
 
+        let m_2_str = kkk[0];
+        let m_3_str = kkk[1];
+
+        let m1 = m_1_str.split(" ");
+        let m2 = m_2_str.split(" ");
+        let m3 = m_3_str.split(" ");
+
+        let arr = null;
+
+        arr = m1;
         for(let i = 0; i < arr.length; i++) {
             if(arr[i] !== "" && arr[i] !== " ") {
                 let n = parseInt(arr[i]);
 
-                addPoint(xxx, n);
-                xxx++;
+                addPoint(xxx_1, n, 1);
+                xxx_1++;
+            }
+        }
+
+        arr = m2;
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] !== "" && arr[i] !== " ") {
+                let n = parseInt(arr[i]);
+
+                addPoint(xxx_2, n, 2);
+                xxx_2++;
+            }
+        }
+
+        arr = m3;
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] !== "" && arr[i] !== " ") {
+                let n = parseInt(arr[i]);
+
+                addPoint(xxx_3, n, 3);
+                xxx_3++;
             }
         }
     }
 };
 
 socket.onerror = function(error) {
-    console.log("Ошибка: " + error.message);
+    console.log("Ошибка");
 };
